45be6206aa7097e0e7166f6bb0c046e6da4afd23
Pablo <p.reinaldo90@gmail.com>
2023-03-27 19:50:05 +0200

    27/03 Commit

--- src/pages/property-list/property-list.js
+++ src/pages/property-list/property-list.js
@@ -1,1 +1,27 @@
-console.log('property-list page')
+import { getPropertyList, getSaleTypeList, getProvinceList } from "./property-list.api";
+import { mapPropertyListFromApiToViewModel } from "./property-list.mappers";
+import { addPropertyRows, setOptions } from "./property-list.helpers";
+import { roomOptions, bathroomOptions, minPriceOptions, maxPriceOptions, } from "./property-list.constants";
+
+
+Promise.all([
+    getPropertyList(),
+    getSaleTypeList(),
+    getProvinceList(),
+]).then(([propertyList, saleTypeList, provinceList]) => {
+    loadPropertyList(propertyList);
+    setOptions(saleTypeList, 'select-sale-type', '¿Qué venta?');
+    setOptions(provinceList, 'select-province', '¿Dónde?');
+    setOptions(roomOptions, 'select-room', '¿Habitaciones?');
+    setOptions(roomOptions, 'select-room', '¿Habitaciones?');
+    setOptions(bathroomOptions, 'select-bathroom', '¿Cuartos de baño?');
+    setOptions(minPriceOptions, 'select-min-price', 'Min (EUR)');
+    setOptions(maxPriceOptions, 'select-max-price', 'Max (EUR)');
+
+});
+
+
+const loadPropertyList = propertyList => {
+    const viewModelPropertyList = mapPropertyListFromApiToViewModel(propertyList);
+    addPropertyRows(viewModelPropertyList);
+};
